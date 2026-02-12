8f53bb1d7ede8f3d51e68336d5a2fa6ce887be83
Alicia Sarrio Colomar <ASARCOL@upv.edu.es>
2026-02-12 12:10:20 +0100

    Creación de la clase User.cs para usuarios de nuestra aplicación.

---
+++ miniVotify.API/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace miniVotify.API.Models
+{
+    public class User
+    {
+        public required int id {  get; set; }
+        public string UserName { get; set; } = string.Empty;
+
+        public required string PasswordHash { get; set; }
+        public required string email { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Boolean IsActive { get; set; }
+
+        public required string Role { get; set; }
+    }
+}
+//Esta clase representa a un usuario con sus atributos que en la base de datos se va a corresponde con una tabla. 
+//Estamos definiendo los atributos que va a tener el usuario que se va a registrar en nuestra aplicación. 
